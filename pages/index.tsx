410d4c8f4601766ab166a400f8d41e108591a641
Hudson <hudsonbirdsong@gmail.com>
2023-02-15 17:32:20 -0600

    Building the avatar image and name UI. Switched to turbopack for faster reloads.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
-// import Image from 'next/image'
+import Image from 'next/image'
+import data from '../data.json'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center mx-auto w-full mt-16">
+      <Image 
+        className="rounded-full mb-4"
+        alt={data.name}
+        src={data.avatar}
+        width={96}
+        height={96}
+      />
+      <h1 className="font-bold text-xl">@{data.name}</h1>
+    </div>
   )
 }
